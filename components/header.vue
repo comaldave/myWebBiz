f53608a75c5c4c82fdc6497014fc819a9f7426cc
Claire Crawford <armande38@gmail.com>
2020-10-24 13:36:19 -0500

    added template to header.vue

---
+++ components/header.vue
@@ -0,0 +1,19 @@
+<template>
+ <header>
+        <div id="main_menu">
+            <span class="logo_area">
+                <a href="index.html" id="logo"><img src="images/logoStarClaireFinal_350.png" id="logo_Claire"></a>
+            </span>
+            <nav class="inner_main_menu">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="portfolio.html">Portfolio</a>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Connect</a></li>
+                </ul>
+            </nav>
+        </div>
+ </header>
+</template> 
